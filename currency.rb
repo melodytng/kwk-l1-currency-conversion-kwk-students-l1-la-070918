0ed5e556feba9d867123eb0913539fa995d8402c
melodytng <melodytang2002@yahoo.com>
2018-07-12 19:52:58 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,50 @@
 # Write your cod here
+def usd_to_eur(usd)
+  eur = usd * 0.86
+  eur = euro.round(2)
+  puts eur
+end
+usd_to_eur(1.0)
+
+def eur_to_usd(eur)
+  usd = eur * 1.17
+  usd = usd.round(2)
+  puts usd
+end
+
+def usd_to_jpy(usd)
+  jpy = usd * 112.53
+  jpy = usd.round(2)
+  puts jpy
+end
+
+def jpy_to_usd(jpy)
+  usd = jpy * 0.0089
+  usd = usd.round(2)
+  puts usd
+end
+
+def usd_to_gbp(usd)
+  gbp = usd * 0.76
+  gbp = gbp.round(2)
+  puts gbp
+end
+
+def gbp_to_usd(gbp)
+  usd = gbp * 1.32
+  usd = usd.round(2)
+  puts usd
+end
+
+# def usd_to_eur(usd)
+#   eur = usd * 0.86
+#   eur = euro.round(2)
+#   puts eur
+# end
+# usd_to_eur(1.0)
+
+# def eur_to_usd(eur)
+#   usd = eur * 1.17
+#   usd = usd.round(2)
+#   puts usd
+# end
